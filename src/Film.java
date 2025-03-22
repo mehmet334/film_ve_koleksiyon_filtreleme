8cfe2f120bb7fabc260705074bec422f60bd3194
Mehmet Ali Tezcan <mehmetalitez33@gmail.com>
2025-03-22 18:38:56 +0300

    ilk

--- src/Film.java
+++ src/Film.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+public class Film {
 
-public class film {
+    private String ad;
+    private int yayinYili;
+    private String tur;
+    private double imdbPuani;
+
+    public Film(String ad, int yayinYili, String tur, double imdbPuani) {
+        this.ad = ad;
+        this.yayinYili = yayinYili;
+        this.tur = tur;
+        this.imdbPuani = imdbPuani;
+    }
+
+    public String getAd() {
+        return ad;
+    }
+
+    public int getYayinYili() {
+        return yayinYili;
+    }
+
+    public String getTur() {
+        return tur;
+    }
+
+    public double getImdbPuani() {
+        return imdbPuani;
+    }
+
+    @Override
+    public String toString() {
+        return ad + " (" + yayinYili + ") - Tür: " + tur + ", IMDb: " + imdbPuani;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,34 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<Film> filmler = new ArrayList<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // 5 farklı film nesnesi ekleniyor
+        filmler.add(new Film("The Prestige", 2006, "Drama", 8.5));;
+        filmler.add(new Film("Whiplash", 2014, "Drama", 8.5));
+        filmler.add(new Film("The Pianist", 2002, "Biography", 8.5));
+        filmler.add(new Film("Parasite", 2019, "Thriller", 8.5));
+        filmler.add(new Film("Amélie", 2001, "Romance", 8.3));
+
+        System.out.println("IMDb'ye Göre : ");
+        filmler.sort(Comparator.comparingDouble(Film::getImdbPuani).reversed());
+        filmler.forEach(System.out::println);
+
+        System.out.println("\nYayın Yılına Göre : ");
+        filmler.sort(Comparator.comparingInt(Film::getYayinYili));
+        filmler.forEach(System.out::println);
+
+        System.out.println("\nDram Filmleri ----");
+        filmFiltrele(filmler, "Drama");
+    }
+
+    // Belirli bir türe göre filtreleme yapan metod
+    public static void filmFiltrele(List<Film> filmler, String tur) {
+        for (Film film : filmler) {
+            if (film.getTur().equalsIgnoreCase(tur)) {
+                System.out.println(film);
+            }
         }
     }
 }
